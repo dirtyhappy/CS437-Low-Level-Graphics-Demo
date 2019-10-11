9a9f51521dbcaab5fbe1eac1befa1ba5ae40583a
Vivek Bigelow <vivbigel@tesla.cs.iupui.edu>
2019-10-11 14:12:36 -0400

    can get mouse position

--- engine.js
+++ engine.js
@@ -1,4 +1,19 @@
 //Basics of an engine  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Mouse objec that tracks that state of the mouse
+function Mouse(){
+	this.updatePos = function(e){
+		this.x = e.pageX;
+		this.y = e.pageY;
+	}
+	this.getX = function(){
+		return this.x;	
+	}
+	this.getY = function(){
+		return this.y;	
+	}
+	//state, clicked or not
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Scene object that holds a canvas
@@ -11,15 +26,23 @@ function Scene(){
 	this.context = this.canvas.getContext("2d");
 	this.canvas.width = 700;
 	this.canvas.height = 500;
-
+	
+	
 	this.start = function(){
 		this.intID = setInterval(localUpdate, 50);
+		document.onmousemove = this.updateMousePos;
 	} // end start
 	
 	//set the background color using r,g,b values
 	this.setBackground = function(r,g,b){
 		var color = 'rgb('+r+','+g+','+b+')';
 		this.canvas.style.backgroundColor = color;
+	}
+	
+	this.updateMousePos = function(e){
+		this.mouseX = e.pageX; //document mouseX
+		this.mouseY = e.pageY; //document mouseY
+		console.log(e.pageX,e.pageY);
 	}
 
 	
@@ -32,4 +55,3 @@ function localUpdate(){
 	update();
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
