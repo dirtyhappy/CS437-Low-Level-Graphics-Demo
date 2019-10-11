e359ffe0f96cc30259b2ce23cedb05f63ce660c0
Vivek Bigelow <vivbigel@tesla.cs.iupui.edu>
2019-10-11 13:03:55 -0400

    Took out set size and set position

--- engine.js
+++ engine.js
@@ -1,4 +1,4 @@
-//Dumbed down engine 
+//Basics of an engine  
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Scene object that holds a canvas
@@ -9,28 +9,12 @@ function Scene(){
 	this.canvas.style.backgroundColor = "black";
 	document.body.appendChild(this.canvas);
 	this.context = this.canvas.getContext("2d");
+	this.canvas.width = 700;
+	this.canvas.height = 500;
 
 	this.start = function(){
 		this.intID = setInterval(localUpdate, 50);
 	} // end start
-	
-	//set the width and height of the canvas
-	this.setSize = function(width,height){
-		this.width = width;
-		this.height = height;
-		this.canvas.width = this.width;
-		this.canvas.height = this.height;
-	} //end setSize
-
-
-	//set the top left corner of the canvas
-	this.setPos = function(left, top){
-		this.left = left;
-		this.top = top;
-		this.canvas.style.MozTransform = "translate(" + left + "px, " + top + "px)";
-		this.canvas.style.WebkitTransform = "translate(" + left + "px, " + top + "px)";
-		this.canvas.style.OTransform = "translate(" + left + "px, " + top + "px)";
-	} //end setPos
 	
 	//set the background color using r,g,b values
 	this.setBackground = function(r,g,b){
@@ -38,8 +22,6 @@ function Scene(){
 		this.canvas.style.backgroundColor = color;
 	}
 
-	this.setSize(500,500);
-	this.setPos(0,0);
 	
 	
 } //end Scene
@@ -49,3 +31,5 @@ function Scene(){
 function localUpdate(){
 	update();
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
